459a9488aa55a6ead541bb72c99722605a1ed66e
nrnbekovv <nrnbekov@gmail.com>
2026-05-19 00:18:45 +0500

    Added pause function

---
+++ Assets/_Game/Scripts/PauseManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
